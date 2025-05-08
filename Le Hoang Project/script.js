2e14a60fe818fa8e56d9d61bc7d4a19d4dd1090b
NAmJox12 <111755334+NAmJox12@users.noreply.github.com>
2025-05-08 20:47:51 +0700

    Add files via upload

---
+++ Le Hoang Project/script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".swiper", {
+    loop: true,  
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },  
+    autoplay: {
+        delay: 3000,  
+        disableOnInteraction: false
+    }
+});
